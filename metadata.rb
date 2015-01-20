4f3178d881b48438bc6df9d048d9de1ed58fa988
Benjamin Pillet <ben_pillet@yahoo.com>
2015-01-19 21:01:28 -0800

    Add code climate

--- metadata.rb
+++ metadata.rb
@@ -4,7 +4,7 @@ maintainer_email 'ben_pillet@yahoo.com'
 license 'Apache 2.0'
 description 'Manage AWS services with Chef'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version '0.1.0'
+version '0.1.1'
 
 # depends 'build-essential'
 
